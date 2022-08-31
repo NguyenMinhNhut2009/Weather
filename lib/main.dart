529b6d8fdff43fb221c610269784de6b467ffe6f
Nguyen Minh Nhut <nhutxngm@gmail.com>
2022-08-31 18:09:53 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+// ignore_for_file: deprecated_member_use
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:mysql1/mysql1.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather/GetLocation.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,68 +21,133 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const FirstOne(),
+      home: const MyHomePage(),
     );
   }
 }
 
-class FirstOne extends StatelessWidget {
-  const FirstOne({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key}) : super(key: key);
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String apiKey = 'XXXXXXXXX';
+  var description;
+  var temp;
+  String city;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("First Screen"),
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Container(
+              child: displayImage(), //Image.asset('images/dayTime.jpg'),
+            ),
+            //SizedBox(height: 50.0,),
+            Container(
+              margin: EdgeInsets.only(top: 50.0),
+              child: Text(
+                'You are in:',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue[500],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      city.toString(),
+                      style: TextStyle(
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[500],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Container(
+                    child: Icon(
+                      Icons.location_on,
+                      color: Colors.red,
+                      size: 35.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 17.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.wb_sunny,
+                  color: Colors.amber,
+                ),
+                title: Text(
+                  'Temperature: ${temp.toString()} C',
+                ),
+                subtitle: Text('Status: ${description.toString()}'),
+              ),
+            ),
+            Container(
+              child: Center(
+                child: FlatButton(
+                  child: Text('Get weather info'),
+                  color: Colors.blue[500],
+                  textColor: Colors.white,
+                  onPressed: () {
+                    setState(() {
+                      getLocation();
+                    });
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-      body: Center(
-          child: ElevatedButton(
-        onPressed: () {},
-        child: Text("Click me"),
-      )),
     );
   }
+
+  displayImage() {
+    var now = DateTime.now();
+    final currentTime = DateFormat.jm().format(now);
+    if (currentTime.contains('AM')) {
+      return Image.asset('images/dayTime.jpg');
+    } else if (currentTime.contains('PM')) {
+      return Image.asset('images/nightTime.jpg');
+    }
+  }
+
+  void getLocation() async {
+    Getlocation getlocation = Getlocation();
+    await getlocation.getCurrentLocation();
+
+    print(getlocation.latitude);
+    print(getlocation.longitude);
+    print(getlocation.city);
+    city = getlocation.city;
+    getTemp(getlocation.latitude, getlocation.longitude);
+  }
+
+  Future<void> getTemp(double lat, double lon) async {
+    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey&units=metric');
+    print(response.body);
+
+    var dataDecoded = jsonDecode(response.body);
+    description = dataDecoded['weather'][0]['description'];
+    temp = dataDecoded['main']['temp'];
+    print(temp);
+  }
 }
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
-//   final String title;
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
